67633961d4a73ac19027f8bb93b3c7943087745f
cellargalaxy <cellargalaxy@gmail.com>
2017-09-28 18:22:11 +0800

    version2备份

--- src/main/java/version2/dataSet/DataSet.java
+++ src/main/java/version2/dataSet/DataSet.java
@@ -5,6 +5,7 @@ import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 import version2.ds.AucCount;
 import version2.ds.DsCount;
+import version2.run.RunParameter;
 
 import java.io.*;
 import java.util.*;
@@ -18,13 +19,13 @@ public class DataSet implements Serializable{
 	private final LinkedList<Id> ids;
 	private final LinkedList<Integer> evidenceNums;
 	private final Map<String, Integer> evidNameToId;
-	
-	public DataSet(File dataSetFile,DataSetParameter dataSetParameter) throws IOException {
-		ids = new LinkedList<Id>();
-		evidenceNums = new LinkedList<Integer>();
-		evidNameToId = new HashMap<String, Integer>();
-		createIds(dataSetFile,dataSetParameter);
-	}
+
+//	public DataSet(File dataSetFile,DataSetParameter dataSetParameter) throws IOException {
+//		ids = new LinkedList<Id>();
+//		evidenceNums = new LinkedList<Integer>();
+//		evidNameToId = new HashMap<String, Integer>();
+//		createIds(dataSetFile,dataSetParameter);
+//	}
 	
 	public DataSet(LinkedList<Id> ids, LinkedList<Integer> evidenceNums, Map<String, Integer> evidNameToId) {
 		this.ids = ids;
@@ -87,42 +88,42 @@ public class DataSet implements Serializable{
 			}
 		}
 	}
-	
-	private final void createIds(File dataSetFile, DataSetParameter dataSetParameter) throws IOException {
-		Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader().parse(new BufferedReader(new InputStreamReader(new FileInputStream(dataSetFile),dataSetParameter.getCoding())));
-		String id = null;
-		int label = -1;
-		LinkedList<double[]> evidences = null;
-		for (CSVRecord record : records) {
-			if (id == null) {
-				id = record.get(dataSetParameter.getIdClo());
-				label = new Integer(record.get(dataSetParameter.getLabelCol()));
-				evidences = new LinkedList<double[]>();
-			} else if (!id.equals(record.get(dataSetParameter.getIdClo()))) {
-				ids.add(new Id(id, evidences, label));
 
-				id = record.get(dataSetParameter.getIdClo());
-				label = new Integer(record.get(dataSetParameter.getLabelCol()));
-				evidences = new LinkedList<double[]>();
-			}
-
-			double[] evidence = {createEvidNum(record.get(dataSetParameter.getEvidCol())), new Double(record.get(dataSetParameter.getACol())), new Double(record.get(dataSetParameter.getBCol()))};
-			evidences.add(evidence);
-		}
-		if (id != null) {
-			ids.add(new Id(id, evidences, label));
-		}
-	}
-
-	private final int createEvidNum(String evidName) {
-		Integer i = evidNameToId.get(evidName);
-		if (i == null) {
-			i = evidNameToId.size() + 1;
-			evidNameToId.put(evidName, i);
-			evidenceNums.add(i);
-		}
-		return i;
-	}
+//	private final void createIds(File dataSetFile, DataSetParameter dataSetParameter) throws IOException {
+//		Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader().parse(new BufferedReader(new InputStreamReader(new FileInputStream(dataSetFile),dataSetParameter.getCoding())));
+//		String id = null;
+//		int label = -1;
+//		LinkedList<double[]> evidences = null;
+//		for (CSVRecord record : records) {
+//			if (id == null) {
+//				id = record.get(dataSetParameter.getIdClo());
+//				label = new Integer(record.get(dataSetParameter.getLabelCol()));
+//				evidences = new LinkedList<double[]>();
+//			} else if (!id.equals(record.get(dataSetParameter.getIdClo()))) {
+//				ids.add(new Id(id, evidences, label));
+//
+//				id = record.get(dataSetParameter.getIdClo());
+//				label = new Integer(record.get(dataSetParameter.getLabelCol()));
+//				evidences = new LinkedList<double[]>();
+//			}
+//
+//			double[] evidence = {createEvidNum(record.get(dataSetParameter.getEvidCol())), new Double(record.get(dataSetParameter.getACol())), new Double(record.get(dataSetParameter.getBCol()))};
+//			evidences.add(evidence);
+//		}
+//		if (id != null) {
+//			ids.add(new Id(id, evidences, label));
+//		}
+//	}
+//
+//	private final int createEvidNum(String evidName) {
+//		Integer i = evidNameToId.get(evidName);
+//		if (i == null) {
+//			i = evidNameToId.size() + 1;
+//			evidNameToId.put(evidName, i);
+//			evidenceNums.add(i);
+//		}
+//		return i;
+//	}
 	
 	public LinkedList<Id> getIds() {
 		return ids;
@@ -136,29 +137,61 @@ public class DataSet implements Serializable{
 		return evidNameToId;
 	}
 	
-	public static final void outputDataSet(DataSet dataSet, File outputDataSet,double subSpaceAuc,DsCount dsCount,Map<String, Integer> evidNameToId, Map<Double, Integer> featureMap, int[] sn, Map<DataSet,double[]> subSpaceMap) throws IOException {
+	public static final void outputDataSet(DataSet dataSet,DataSet subDataSet, File outputDataSet,RunParameter runParameter,
+	                                       double subSpaceAuc,DsCount dsCount,AucCount aucCount,
+	                                       Map<String, Integer> evidNameToId,
+	                                       Map<Double, Integer> featureMap,
+	                                       Map<DataSet,double[]> subSpaceMap) throws IOException {
 		outputDataSet.getParentFile().mkdirs();
 		CSVFormat csvFormat =  CSVFormat.DEFAULT.withRecordSeparator(CSV_Record_SEPARATOR);
 		CSVPrinter csvPrinter = new CSVPrinter(new FileWriter(outputDataSet), csvFormat);
-		csvPrinter.printRecord("id","label","A","B");
-		for (Id id : dataSet.getIds()) {
+		csvPrinter.printRecord("id","label","A","B","AB");
+		for (Id id : subDataSet.getIds()) {
 			double[] doubles=dsCount.countDs(id);
-			csvPrinter.printRecord(id.getId(),id.getLabel(),doubles[1],doubles[2]);
+			csvPrinter.printRecord(id.getId(),id.getLabel(),doubles[1],doubles[2],1-doubles[1]-doubles[2]);
 		}
 		
-		csvPrinter.printRecord("");
+		csvPrinter.printRecord();
+		csvPrinter.printRecord("fnMin","sn","测试集比例","缺失集比例","标签1比例");
+		csvPrinter.printRecord(runParameter.getFnMin(),Arrays.toString(runParameter.getSn()),runParameter.getTest(),runParameter.getMiss(),runParameter.getLabel1());
+		
+		Map<String,Integer> evidenceCountMap=new HashMap<String, Integer>();
+		for (Id id : dataSet.getIds()) {
+			List<Integer> evidNums=new LinkedList<Integer>();
+			for (double[] doubles : id.getEvidences()) {
+				evidNums.add((int)doubles[0]);
+			}
+			Collections.sort(evidNums);
+			String string;
+			if (id.getLabel()== Id.LABEL_0) {
+				string=evidNums.size()+"个特征0标签"+evidNums.toString();
+			}else {
+				string=evidNums.size()+"个特征1标签"+evidNums.toString();
+			}
+			Integer count=evidenceCountMap.get(string);
+			if (count==null) {
+				count=new Integer(0);
+			}
+			evidenceCountMap.put(string,count+1);
+		}
+		csvPrinter.printRecord();
+		for (Map.Entry<String, Integer> entry : evidenceCountMap.entrySet()) {
+			csvPrinter.printRecord(entry.getKey(),(double)entry.getValue()/dataSet.getIds().size());
+		}
+		
+		csvPrinter.printRecord();
 		csvPrinter.printRecord("AUC",subSpaceAuc);
 		
-		csvPrinter.printRecord("");
-		csvPrinter.printRecord("证据编号与证据名");
+		csvPrinter.printRecord();
+		csvPrinter.printRecord("证据编号","证据名");
 		for (Map.Entry<String, Integer> entry : evidNameToId.entrySet()) {
 			csvPrinter.printRecord(entry.getValue(),entry.getKey());
 		}
 		
-		csvPrinter.printRecord("");
+		csvPrinter.printRecord();
 		csvPrinter.printRecord("证据合成",dsCount.getName());
 		
-		csvPrinter.printRecord("");
+		csvPrinter.printRecord();
 		int len=featureMap.get(Double.MAX_VALUE);
 		int i=0;
 		csvPrinter.printRecord("重要特征");
@@ -170,14 +203,10 @@ public class DataSet implements Serializable{
 			}
 		}
 		
-		csvPrinter.printRecord("");
-		csvPrinter.printRecord("子空间sn");
-		csvPrinter.printRecord(Arrays.toString(sn));
-		
-		csvPrinter.printRecord("");
-		csvPrinter.printRecord("子空间,染色体");
+		csvPrinter.printRecord();
+		csvPrinter.printRecord("子空间","子空间AUC","染色体");
 		for (Map.Entry<DataSet, double[]> entry : subSpaceMap.entrySet()) {
-			csvPrinter.printRecord(entry.getKey().getEvidenceNums(),Arrays.toString(entry.getValue()));
+			csvPrinter.printRecord(entry.getKey().getEvidenceNums(),aucCount.countOrderAuc(entry.getKey(),entry.getValue()),Arrays.toString(entry.getValue()));
 		}
 		
 		csvPrinter.close();
--- src/main/java/version2/run/Run.java
+++ src/main/java/version2/run/Run.java
@@ -86,7 +86,7 @@ public class Run {
 			AucCount aucCount1=new Auc(dsCount1);
 			hereditary.evolution(hereditaryParameter,parentChrosChoose,aucCount1);
 			
-			System.out.println(dsCount1.getName()+" ds auc:"+hereditary.getMaxAuc());/////////////////////////////////////
+			System.out.println(dsCount1.getName()+"\tds auc:"+hereditary.getMaxAuc());/////////////////////////////////////
 			
 			if (hereditary.getMaxAuc()>dsAuc) {
 				dsAuc=hereditary.getMaxAuc();
@@ -150,7 +150,10 @@ public class Run {
 		double trainSubSpaceAuc=subSpaceAucCount.countAuc(trainSubSpaceDataSet);
 		System.out.println("trainSubSpaceDataSet auc:"+trainSubSpaceAuc);
 		
-		DataSet.outputDataSet(trainSubSpaceDataSet,trainOutputFile,trainSubSpaceAuc,dsCount,trainDataSet.getEvidNameToId(),featureMap,sn,trainSubSpaceMap);
+		DataSet.outputDataSet(trainDataSet,trainSubSpaceDataSet,trainOutputFile,runParameter,
+				trainSubSpaceAuc,dsCount,aucCount,
+				trainDataSet.getEvidNameToId(),
+				featureMap,trainSubSpaceMap);
 		
 		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		
@@ -165,107 +168,11 @@ public class Run {
 		DataSet testSubSpaceDataSet=SubSpace2DataSet.subSpace2DataSet(testSubSpaceMap,dsCount);
 		double testSubSpaceAuc=subSpaceAucCount.countAuc(testSubSpaceDataSet);
 		System.out.println("testSubSpaceDataSet auc:"+testSubSpaceAuc);
-		DataSet.outputDataSet(testSubSpaceDataSet,testOutputFile,testSubSpaceAuc,dsCount,testDataSet.getEvidNameToId(),featureMap,sn,testSubSpaceMap);
+		DataSet.outputDataSet(testDataSet,testSubSpaceDataSet,testOutputFile,runParameter,
+				testSubSpaceAuc,dsCount,aucCount,
+				testDataSet.getEvidNameToId(),
+				featureMap,testSubSpaceMap);
 	}
 	
-	public static final void run() throws IOException, ClassNotFoundException {
-		DataSetParameter dataSetParameter=new DataSetParameter("utf-8",0,5,1,2,3);
-		DataSet trainDataSet = new DataSet(new File("/media/cellargalaxy/根/内/大学/xi/dachuang/dataSet/trainAll.csv"),dataSetParameter);
-		
-		DsCount dsCount=null;
-		AucCount aucCount=null;
-		double dsAuc=-1;
-		DsCount[] dsCounts={new TeaDsCount()};
-		Hereditary hereditary=new Hereditary(trainDataSet);
-		HereditaryParameter hereditaryParameter=new HereditaryParameter();
-		hereditaryParameter.setIterNum(500);
-		hereditaryParameter.setSameNum(100);
-		ParentChrosChoose parentChrosChoose=new RouletteParentChrosChoose();
-		for (DsCount dsCount1 : dsCounts) {
-			AucCount aucCount1=new Auc(dsCount1);
-			hereditary.evolution(hereditaryParameter,parentChrosChoose,aucCount1);
-			
-			System.out.println("ds auc:"+hereditary.getMaxAuc());/////////////////////////////////////
-			
-			if (hereditary.getMaxAuc()>dsAuc) {
-				dsAuc=hereditary.getMaxAuc();
-				dsCount=dsCount1;
-				aucCount=aucCount1;
-			}
-		}
-		System.out.println("--------------------------------------------------------");
-		System.out.println();
-		
-		FeatureSeparation featureSeparation=new MedianFeatureSeparation();
-		Map<Double, Integer> featureMap= FeatureSelection.featureSelection(aucCount,hereditaryParameter,parentChrosChoose,featureSeparation,0.01,hereditary);
-		
-		int len=featureMap.get(Double.MAX_VALUE);
-		List<Integer> features=new LinkedList<Integer>();
-		double[] impros=new double[len];
-		int i=0;
-		System.out.println("重要特征:");
-		for (Map.Entry<Double, Integer> entry : featureMap.entrySet()) {
-			features.add(entry.getValue());
-			impros[i]=entry.getKey();
-			
-			System.out.println(entry.getValue()+" "+entry.getKey());
-			i++;
-			if (i>=len) {
-				break;
-			}
-		}
-		double trainFeatureAuc=aucCount.countAuc(trainDataSet,features);
-		System.out.println("训练集特征选择 auc:"+trainFeatureAuc);
-		System.out.println("--------------------------------------------------------");
-		System.out.println();
-		
-		int[] sn = {1, 2, 3, 4};
-		ImprotenceAdjust improtenceAdjust=new PowerImprotenceAdjust();
-		List<List<Integer>> subSpaces = SubSpace.createSubSpaces(features,impros,sn,25,improtenceAdjust);
-		System.out.println("子空间:");
-		for (List<Integer> subSpace : subSpaces) {
-			System.out.println(subSpace);
-		}
-		System.out.println("--------------------------------------------------------");
-		System.out.println();
-		
-		Map<DataSet,double[]> trainSubSpaceMap=new HashMap<DataSet, double[]>();
-		Iterator<List<Integer>> iterator = subSpaces.iterator();
-		while (iterator.hasNext()) {
-			List<Integer> subSpace = iterator.next();
-			DataSet dataSet=hereditary.evolution(hereditaryParameter,parentChrosChoose,aucCount,subSpace);
-			if (hereditary.getMaxAuc() >= trainFeatureAuc) {
-				trainSubSpaceMap.put(dataSet,hereditary.getMaxChro());
-				System.out.println("训练子空间训练集进化AUC:" + hereditary.getMaxAuc());
-			}
-		}
-		if (trainSubSpaceMap.size() == 0) {
-			trainSubSpaceMap.put(hereditary.evolution(hereditaryParameter,parentChrosChoose,aucCount,features),hereditary.getMaxChro());
-			System.out.println("全子空间训练集进化AUC:" + trainFeatureAuc);
-		}
-		
-		DataSet trainSubSpaceDataSet=SubSpace2DataSet.subSpace2DataSet(trainSubSpaceMap,dsCount);
-		Auc subSpaceAucCount=new Auc(new AverageDsCount());
-		double trainSubSpaceAuc=subSpaceAucCount.countAuc(trainSubSpaceDataSet);
-		System.out.println("trainSubSpaceDataSet auc:"+trainSubSpaceAuc);
-		
-		DataSet.outputDataSet(trainSubSpaceDataSet,new File("/home/cellargalaxy/trainSubSpaceDataSet.csv"),trainSubSpaceAuc,dsCount,trainDataSet.getEvidNameToId(),featureMap,sn,trainSubSpaceMap);
-		
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		
-		DataSet testDataSet = new DataSet(new File("/media/cellargalaxy/根/内/大学/xi/dachuang/dataSet/testAll.csv"),dataSetParameter);
-		Map<DataSet,double[]> testSubSpaceMap=new HashMap<DataSet, double[]>();
-		for (Map.Entry<DataSet, double[]> entry : trainSubSpaceMap.entrySet()) {
-			List<Integer> subSpace=entry.getKey().getEvidenceNums();
-			DataSet dataSet=CloneObject.clone(testDataSet);
-			dataSet.removeNotEqual(subSpace);
-			testSubSpaceMap.put(dataSet,entry.getValue());
-			System.out.println("测试子空间训练集进化AUC:" + aucCount.countAuc(dataSet));
-		}
-		DataSet testSubSpaceDataSet=SubSpace2DataSet.subSpace2DataSet(testSubSpaceMap,dsCount);
-		double testSubSpaceAuc=subSpaceAucCount.countAuc(testSubSpaceDataSet);
-		System.out.println("testSubSpaceDataSet auc:"+testSubSpaceAuc);
-		DataSet.outputDataSet(testSubSpaceDataSet,new File("/home/cellargalaxy/testSubSpaceDataSet.csv"),testSubSpaceAuc,dsCount,testDataSet.getEvidNameToId(),featureMap,sn,testSubSpaceMap);
-	}
 	
 }
